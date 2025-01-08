ad761273623313a3f824f2b826be8b540f85b233
Marcin Koziński <mkozinski@mozilla.com>
2025-01-08 11:47:25 +0100

    build(git): avoid calling bash from kotlin called from bash

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -8,5 +8,11 @@ if ! command -v kotlinc > /dev/null 2>&1; then
     exit 1
 fi
 
-
-scripts/sort-version-catalog.main.kts
+VERSION_CATALOG_PATH=gradle/libs.versions.toml
+# If changes to version catalog are staged..
+if git diff --cached --name-only | grep -q "^$VERSION_CATALOG_PATH$"; then
+  # .. sort it..
+  scripts/sort-version-catalog.main.kts "$VERSION_CATALOG_PATH"
+  # .. and stage it.
+  git add -- "$VERSION_CATALOG_PATH"
+fi
--- scripts/sort-version-catalog.main.kts
+++ scripts/sort-version-catalog.main.kts
@@ -1,17 +1,6 @@
 #!/usr/bin/env kotlin
-import java.io.File
-import kotlin.system.exitProcess
 
-fun bash(command: String): Process {
-    return ProcessBuilder("/usr/bin/env", "bash", "-c", command).start()
-}
-
-val versionCatalog = File("gradle/libs.versions.toml")
-if (bash("git diff --cached --name-only | grep -q \"^${versionCatalog.path}\$\"").waitFor() != 0) {
-    // Version catalog wasn't modified. Nothing to do.
-    exitProcess(0)
-}
-
+val versionCatalog = java.io.File(args[0])
 val sorted = versionCatalog.readText()
     // Split into sections on the empty line.
     .split("\n\n")
@@ -26,6 +15,3 @@ val sorted = versionCatalog.readText()
 
 // Overwrite the version catalog with the sorted version.
 versionCatalog.writeText(sorted)
-
-bash("git add -- ${versionCatalog.path}").waitFor()
-exitProcess(0)
