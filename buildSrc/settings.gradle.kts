43851ce4a2fa579215060ee8cf83681759673a80
Marcin Koziński <mkozinski@mozilla.com>
2025-01-23 10:34:46 +0100

    chore(docs): clean up an obsolete comment

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,7 +1,3 @@
-// From Gradle docs:
-// > We recommend that you also create a buildSrc/settings.gradle.kts file, which you may leave empty.
-// Source: https://docs.gradle.org/current/userguide/custom_plugins.html#sec:precompiled_plugins
-
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
